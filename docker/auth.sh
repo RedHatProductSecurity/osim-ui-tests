eb0688282bb5082ba226fe6ff321b971b0aa6f15
Alvaro Tinoco <atinocom@redhat.com>
2025-07-10 16:37:36 +0200

    Fix keytab principal regex to allow hyphens

--- docker/auth.sh
+++ docker/auth.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-principal="$( klist -kt /krb5/krb5.keytab | grep -Eo -m1 '\w+@[A-Z.]+' )"
+principal="$( klist -kt /krb5/krb5.keytab | grep -Eo -m1 '[a-z.-]+@[A-Z.]+' )"
 
 kinit -k -t /krb5/krb5.keytab $principal
 klist -c /tmp/cache
